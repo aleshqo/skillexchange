d0bc2d45531b4ec3ebd657d2bab7a238569585a8
Бахарев Алексей Владиславович <AVlaBakharev@sberbank.ru>
2023-08-14 15:20:51 +0700

    init commit

---
+++ src/test/java/ru/programmingweek/skillexchange/SkillExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.programmingweek.skillexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
